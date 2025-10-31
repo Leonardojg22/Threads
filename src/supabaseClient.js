d717ffb8e42969c270bdea96d8fc3aeb07b0d22e
Leonardojg22 <leonardojg686@gmail.com>
2025-10-30 20:20:14 -0500

    feature: Use enviroment variables for Supabase Keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Reemplaza estas variables con tus claves reales de Supabase
-const supabaseUrl = 'https://swtaxcbvohjviywnytud.supabase.co'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3dGF4Y2J2b2hqdml5d255dHVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MzAxODQsImV4cCI6MjA3NzMwNjE4NH0.kXdqMLsxqr3CWgpCGUJ1EIpK7E_Dw5OgLbqrjJkdNOM'; 
+// Corregido: Lee variables de entorno con el prefijo VITE_
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; 
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
